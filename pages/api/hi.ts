bffb43f7676022c801da5007d57b6a7d8d3debed
Josh Pollock <jpollock412@gmail.com>
2021-01-29 18:28:04 -0500

    methods

--- pages/api/hi.ts
+++ pages/api/hi.ts
@@ -2,7 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 //Says Hi to Roy
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	res.setHeader("Content-Type", "application/json");
-	res.setHeader("Cache-Control", "s-maxage=360");
-	res.status(200).json({ hi: "Roy" });
+	switch (req.method) {
+		case "GET": {
+			res.setHeader("Content-Type", "application/json");
+			res.setHeader("Cache-Control", "s-maxage=360");
+			res.status(200).json({ hi: "Roy" });
+			break;
+		}
+		default:
+			res.status(405).json({ error: "Not implimented" });
+			break;
+	}
 };
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,16 +3,24 @@ import { NextApiRequest } from "next";
 import factory from "../../lib/factory";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	let app = await factory(undefined);
-	let { email, password } = req.body;
-	try {
-		let user = await app.loginUser(email, password);
-		if (user) {
-			let session = await app.authService.startUserSession(user);
-			res.status(200).json({ session });
-		} else {
-			res.status(403).json({ error: "User not found" });
+	switch (req.method) {
+		case "POST": {
+			let { email, password } = req.body;
+			try {
+				let user = await app.loginUser(email, password);
+				if (user) {
+					let session = await app.authService.startUserSession(user);
+					res.status(200).json({ session });
+				} else {
+					res.status(403).json({ error: "User not found" });
+				}
+			} catch (error) {
+				res.status(403).json({ error });
+			}
+			break;
 		}
-	} catch (error) {
-		res.status(403).json({ error });
+		default:
+			res.status(405).json({ error: "Not implimented" });
+			break;
 	}
 };
