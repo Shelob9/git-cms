2750360bd3dd84ba3e0326b21aa87de3766e35cb
Josh Pollock <jpollock412@gmail.com>
2021-02-04 12:38:16 -0500

    export login/logout ui

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,15 @@ import GitApi from "./lib/git/GitApi";
  * UI
  */
 import useSession from "./hooks/useSession";
-import Header from "./components/Header";
-export { applicationService, GitApi, useSession, Header };
+//import Header from "./components/Header";
+import LoginPage from "./pages/LoginPage";
+import LogoutPage from "./pages/LogoutPage";
+import RegistrationPage from "./pages/RegistrationPage";
+export {
+	applicationService,
+	GitApi,
+	useSession,
+	RegistrationPage,
+	LogoutPage,
+	LoginPage
+};
