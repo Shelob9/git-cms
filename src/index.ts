81248c0ef48ac440aab731d15cb864be70c14e5d
Josh Pollock <jpollock412@gmail.com>
2021-02-27 13:56:31 -0500

    inline docs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { hashString } from "./lib/services/authService";
-import { hashPassword, checkPassword } from "./lib/password";
-import { encrypt, decrypt } from "./lib/encryptDecrypt";
-import { createJwtToken, decodeJwtToken } from "./lib/jwt";
 /**
  * CMS
  */
@@ -11,10 +7,17 @@ import GitApi from "./lib/git/GitApi";
  * UI
  */
 import useSession from "./hooks/useSession";
-//import Header from "./components/Header";
+
 import LoginPage from "./pages/LoginPage";
 import LogoutPage from "./pages/LogoutPage";
 import RegistrationPage from "./pages/RegistrationPage";
+/**
+ * Utility functions
+ */
+import { hashString } from "./lib/services/authService";
+import { hashPassword, checkPassword } from "./lib/password";
+import { encrypt, decrypt } from "./lib/encryptDecrypt";
+import { createJwtToken, decodeJwtToken } from "./lib/jwt";
 export {
 	applicationService,
 	GitApi,
